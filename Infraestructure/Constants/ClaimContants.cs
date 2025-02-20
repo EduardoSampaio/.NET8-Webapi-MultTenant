778813f127278ed8d4529c6a7e3b8f50f453a2fc
Eduardo <edwardviana@gmail.com>
2025-02-19 21:56:23 -0300

    Constants done

---
+++ Infraestructure/Constants/ClaimContants.cs
@@ -0,0 +1,8 @@
+﻿namespace Infraestructure.Constants
+{
+    public static class ClaimContants
+    {
+        public const string Tenant = "tenant";
+        public const string Permisson = "permisson";
+    }
+}
---
+++ Infraestructure/Constants/RoleContants.cs
@@ -0,0 +1,11 @@
+﻿namespace Infraestructure.Constants;
+
+public class RoleContants
+{
+    public const string Admin = nameof(Admin);
+    public const string Basic = nameof(Basic);
+
+    public static IReadOnlyCollection<string> DefaultRoles => [Admin, Basic];
+
+    public static bool IsDefaultRole(string roleName) => DefaultRoles.Contains(roleName);
+}
