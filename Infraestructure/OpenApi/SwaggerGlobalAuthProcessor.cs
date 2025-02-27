46ac76cff4161994162bfe8ce35d83708de9d99a
Eduardo <edwardviana@gmail.com>
2025-02-26 22:30:45 -0300

    swagger configuration

---
+++ Infraestructure/OpenApi/SwaggerGlobalAuthProcessor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Namotion.Reflection;
+using NSwag.Generation.AspNetCore;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.OpenApi
+{
+    public class SwaggerGlobalAuthProcessor(string scheme) : IOperationProcessor
+    {
+        private readonly string _scheme = scheme;
+
+        public SwaggerGlobalAuthProcessor(): this(JwtBearerDefaults.AuthenticationScheme)
+        {
+            
+        }
+
+        public bool Process(OperationProcessorContext context)
+        {
+            var list = ((AspNetCoreOperationProcessorContext)context)
+                .ApiDescription.ActionDescriptor.TryGetPropertyValue<IList<object>>("EndpointMetadata") as List<object>;
+
+            if (list is not null)
+            {
+                if(list.OfType<AllowAnonymousAttribute>().Any())
+                {
+                    return true;
+                }
+
+                if(context.OperationDescription.Operation.Security.Count == 0)
+                {
+                    (context.OperationDescription.Operation.Security ??= [])
+                        .Add(new NSwag.OpenApiSecurityRequirement
+                        {
+                            { 
+                                _scheme,
+                                Array.Empty<string>()
+                            }
+                        });
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static T TryGetPropertyValue<T>(this object obj, string propertyName, T defaultValue = default)
+        {
+            var property = obj.GetType().GetProperty(propertyName);
+            if (property is not null)
+            {
+                return (T)property.GetValue(obj);
+            }
+            return defaultValue;
+        }
+    }
+}
---
+++ Infraestructure/OpenApi/SwaggerHeaderAttributeProcessor.cs
@@ -0,0 +1,41 @@
+﻿using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using System.Reflection;
+using NJsonSchema;
+
+namespace Infraestructure.OpenApi
+{
+    public class SwaggerHeaderAttributeProcessor : IOperationProcessor
+    {
+        public bool Process(OperationProcessorContext context)
+        {
+            if (context.MethodInfo.GetCustomAttribute(typeof(SwaggerHeaderAttribute)) is SwaggerHeaderAttribute swaggerHeader)
+            {
+                var parameters = context.OperationDescription.Operation.Parameters;
+
+                var existingParameter = parameters.FirstOrDefault(p => p.Name == swaggerHeader.HeaderName && p.Kind == NSwag.OpenApiParameterKind.Header);
+
+                if (existingParameter is not null)
+                {
+                    parameters.Remove(existingParameter);
+                }
+
+                parameters.Add(new NSwag.OpenApiParameter
+                {
+                    Name = swaggerHeader.HeaderName,
+                    Kind = NSwag.OpenApiParameterKind.Header,
+                    Description = swaggerHeader.Description,
+                    Default = swaggerHeader.DefaultValue,
+                    IsRequired = true,
+                    Schema = new JsonSchema
+                    {
+                        Type = JsonObjectType.String,
+                        Default = swaggerHeader.DefaultValue,
+                    },
+
+                });
+            }
+            return true;
+        }
+    }
+}
